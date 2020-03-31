0c6e69e3f9705653abbe2ac702bb59d7bf6e5e5c
Brenda Butler <brendabutler@brendas-imac.home>
2020-03-30 20:09:21 -0700

    Add HTML, CSS, and JS files for lightbox

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// Initialize Lightbox
+
+lightbox.option({
+  'maxWidth': 800,
+  'maxHeight': 500,
+  'showImageNumberLabel': false,
+  'wrapAround': true
+});
+
+//
+
+var lightboxImages = $('.lightbox-main');
+
+// Initiate Live Search on Keyup
+
+$('.search-bar').on('keyup', 'input', function() {
+
+// Translate user's (case insensitive) search input to RegExp
+
+  let searchTerm = $(this).val();
+  let regexSearchTerm = new RegExp(`${searchTerm}`, 'i');
+
+// Iterate through images to test RegExp against each caption
+
+  lightboxImages.each(function(index){
+    let caption = $(this).attr('data-title');
+    if (regexSearchTerm.test(caption)) {
+
+      // Show only images that match
+
+      $(this).show();
+    } else {
+      $(this).hide();
+    };
+  });
+});
